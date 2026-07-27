0618c94245d83bf960dbb68737cbc47066b56dd4
arshiasukheja <arshia.sukheja@gmail.com>
2026-07-27 09:17:52 -0700

    Time: 5 ms (38.6%), Space: 45 MB (39.67%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,26 +1,26 @@
 class Solution {
     public boolean isPalindrome(String s) {
         s=s.toLowerCase();
-        StringBuilder sb = new StringBuilder(); 
-        for (char ch: s.toCharArray()){
+        StringBuilder sb= new StringBuilder();
+        
+        for (char ch : s.toCharArray()){
             if (Character.isLetter(ch) || Character.isDigit(ch)){
                 sb.append(ch);
             }
         }
-
-        s=sb.toString();
-
+        s=sb.toString(); 
         int l=0;
         int r=s.length()-1;
 
-        while (l<=r){
-            if (s.charAt(l)!=s.charAt(r)){
-                return false;
-            }
+       while (l<=r){
+    
+        if (s.charAt(l)!=s.charAt(r)){
+            return false;
+        }
             l++;
             r--;
-        }
-
-return true;
+       }
+       
+     return true;  
     }
 }
