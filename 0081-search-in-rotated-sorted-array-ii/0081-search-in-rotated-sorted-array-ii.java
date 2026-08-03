99e89ec0046b1e52e342739739e5727aadf21f74
arshiasukheja <arshia.sukheja@gmail.com>
2026-08-02 23:10:18 -0700

    Time: 0 ms (100%), Space: 45 MB (66.74%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,37 +1,32 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-         int l=0;
-     int r=nums.length-1;
-     while (l<=r){
-        int mid=l+(r-l)/2;
-        if (nums[mid]==target ) return true;
+        int l=0;
+        int r=nums.length-1;
+        while (l<=r){
+            int mid=l+(r-l)/2;
+            if (nums[mid]==target) return true ;
 
- if (nums[l] == nums[mid] && nums[mid] == nums[r]) {
+            if (nums[l]==nums[mid] && nums[mid]==nums[r]){
                 l++;
                 r--;
                 continue;
             }
-        // check if left array is sorted 
-        if (nums[l]<=nums[mid]){
-            if (target>=nums[l] && target<nums[mid]){
-                r=mid-1;
+
+            if (nums[l]<=nums[mid]){
+                if (nums[l]<=target && nums[mid]>target){
+                    r=mid-1;
+                } else {
+                    l=mid+1;
+                }
             } else {
-                l=mid+1;
-            }
-        } 
-        // check if right array is sorted 
-        else {
-            if (target>nums[mid] && target<=nums[r]){
-                l=mid+1;
-            } else {
-                r=mid-1;
+                  if (nums[mid]<target && nums[r]>=target){
+                    l=mid+1;
+                } else {
+                    r=mid-1;
+                }
             }
         }
 
-
-
-     }
-
-     return false;
+        return false;
     }
 }
