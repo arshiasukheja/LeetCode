989a98d85221354ef6ed28894a7ec249bde0e37e
arshiasukheja <arshia.sukheja@gmail.com>
2026-09-08 10:05:07 +0530

    Time: 2 ms (99.72%), Space: 45.2 MB (41.79%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,31 +1,32 @@
 class Solution {
 
-    public void backtrack(int[] nums , int i, List<List<Integer>> result , List<Integer> current){
+    public void backtrack(int[] nums, int idx, List<List<Integer>>result , List<Integer>current){
 
-        if (i==nums.length){
-            result.add(new ArrayList<Integer>(current));
+        if (idx==nums.length){
+            result.add(new ArrayList<Integer> (current));
             return;
         }
-
-        current.add(nums[i]);
-        backtrack(nums, i+1, result,current);
-
+        
+        current.add(nums[idx]);
+        backtrack(nums, idx+1, result, current);
+        
         current.remove(current.size()-1);
-        while (i+1 < nums.length && nums[i]==nums[i+1]){
-            i++;
+        while (idx+1<nums.length && nums[idx]==nums[idx+1]){
+            idx++;
         }
-        backtrack(nums, i+1 , result, current);
-
+        backtrack(nums, idx+1, result, current);
     }
+
 
 
     public List<List<Integer>> subsetsWithDup(int[] nums) {
         Arrays.sort(nums);
-        List<List<Integer>>result=new ArrayList<>();
-        List<Integer>current=new ArrayList<>();
-        int index=0;
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current = new ArrayList<>();
 
-        backtrack (nums, index, result, current);
+        int idx=0;
+        backtrack(nums, idx, result , current);
+
         return result;
     }
 }
