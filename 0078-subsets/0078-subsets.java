a2d006c68e5be4a0eb031064fa497cb45b4c17fe
arshiasukheja <arshia.sukheja@gmail.com>
2026-09-08 09:53:07 +0530

    Time: 1 ms (86.89%), Space: 44.3 MB (38.56%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,25 +1,27 @@
 class Solution {
 
-    public void backtrack (int[] nums, List<List<Integer>>result, List<Integer> current, int i ){
-        if (i==nums.length) {
-            result.add(new ArrayList<>(current));
+    public void backtrack(int[] nums,  List<List<Integer>> result, List<Integer> current, int idx){
+        if (idx==nums.length) {
+            result.add(new ArrayList<Integer>(current));
             return;
         }
 
-        current.add(nums[i]);
-        backtrack(nums,result,current,i+1);
-
+        current.add(nums[idx]);
+        backtrack(nums, result, current, idx+1);
         current.remove(current.size()-1);
-        backtrack(nums,result,current,i+1);
-
+        backtrack(nums, result, current, idx+1);
     }
 
-    public List<List<Integer>> subsets(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>(); // store subsets 
-        List<Integer> current= new ArrayList<>(); // each subset 
-        int index=0;
 
-        backtrack(nums,result,current,index);     
-        return result;   
+
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> current= new ArrayList<>();
+        int idx=0;
+
+        backtrack(nums, result, current, idx);
+        return result;
+
     }
 }
